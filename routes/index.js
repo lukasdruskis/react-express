b0fd72c211cdbc6613c0bfb59d11be8d1b9110e4
Vytautas Morkūnas <vytautas.morkunas@panko.lt>
2019-02-06 14:27:05 +0200

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+const MongoClient = require('mongodb').MongoClient
+
+const mongoUrl = 'mongodb://localhost:27017/'
+
+router.get('/', (req, res, next) => {
+
+  MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, client) => {
+    if (err) throw err
+  
+    const db = client.db("database")
+  
+    db.collection("messages").findOne({}, (err, result) => {
+      if (err) throw err
+      
+      res.render('index', {title: 'TINKLAPIS', message: result.message})
+    })
+  })
+
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('express-handlebars')
+const MongoClient = require('mongodb').MongoClient
+const routes = require('./routes/index')
+
+const app = express()
+
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts'}))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+
+const mongoUrl = 'mongodb://localhost:27017/'
+
+// Find all documents
+//
+// MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, client) => {
+//   if (err) throw err
+
+//   const db = client.db("database")
+//   db.collection("users").find({}).toArray((err, result) => {
+//     if (err) throw err
+//     console.log(result)
+//   })
+//   console.log(`Connected to DB at ${mongoUrl}`)
+// })
+
+app.use('/', routes)
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server started on ${port}`))
