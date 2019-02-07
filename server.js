4a41c99f32ba8f557dc032aaf5900bb62cc8aef6
Vytautas Morkūnas <vytautas.morkunas@panko.lt>
2019-02-07 10:36:26 +0200

    nu

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const routes = require('./routes/index')
 
 const app = express()
 
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts'}))
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'layout',
+  layoutsDir: __dirname + '/views/layouts'
+}))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
